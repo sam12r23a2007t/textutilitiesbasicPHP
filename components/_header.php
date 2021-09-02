3a7198b27a6e759b10b23a237f8671457aaa879a
abc <samratsaha23122007@gmail.com>
2021-09-02 13:11:44 +0530

    Fixes some redirecting problem

--- components/_header.php
+++ components/_header.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="home.php">TextUtilitiesBasicPHP</a>
+  <a class="navbar-brand" href="index.php">TextUtilitiesBasicPHP</a>
 <ul class="nav justify-content-center">
   <li class="nav-item">
     <a class="nav-link active text-light" href="index.php">Home</a>
